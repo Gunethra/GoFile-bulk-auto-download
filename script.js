b764ed1c9fbbde1eece43eb464890d2fc4e709ae
Gunethra <117150479+Gunethra@users.noreply.github.com>
2024-03-10 18:05:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         GoFile Bulk Downloader
+// @version      0.1
+// @description  Download all files from a folder on Gofile without having to click each and every download button.
+// @match        *://gofile.io/*
+// @run-at       document-end
+// ==/UserScript==
+
+const links = document.querySelectorAll('a[href*="download"]');
+
+// Print all links in the console
+// links.forEach(link => {
+//     console.log(link.href);
+// });
+
+let index = 0;
+const interval = setInterval(() => {
+    if (index < links.length) {
+        links[index].click();
+        index++;
+    } else {
+        clearInterval(interval);
+    }
+}, 5000);
